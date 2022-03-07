e544ba569fe5683c2a94c9d3a51df1d27256d0da
dekapx <xkapil@gmail.com>
2022-03-07 23:28:59 +0000

    code cleanup

---
+++ contact-service/src/main/java/com/dekapx/apps/ContactServiceApp.java
@@ -0,0 +1,13 @@
+package com.dekapx.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactServiceApp.class, args);
+	}
+
+}
