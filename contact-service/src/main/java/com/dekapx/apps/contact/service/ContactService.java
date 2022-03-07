e01c14a40cf64b28fc262a98536334260e77567f
dekapx <xkapil@gmail.com>
2022-03-07 23:13:53 +0000

    code cleanup

--- contact-service/src/main/java/com/dekapx/apps/contact/service/ContactService.java
+++
@@ -1,9 +0,0 @@
-package com.dekapx.apps.contact.service;
-
-import com.dekapx.apps.contact.domain.Contact;
-
-public interface ContactService {
-    Contact findById(Long id);
-
-    Contact save(Contact contact);
-}
--- contact-service/src/main/java/com/dekapx/apps/contact/service/SyncService.java
+++
@@ -1,5 +0,0 @@
-package com.dekapx.apps.contact.service;
-
-public interface SyncService {
-    void sendEvent(String event);
-}
